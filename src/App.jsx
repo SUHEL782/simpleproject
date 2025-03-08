a8e4c7a86c35ed1f5effe0e0edb7c0240286f1fc
suhel <thecomputersk@gamil.com>
2025-03-08 13:22:11 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1>This project I try to assign docker file and create container!</h1>
+    </>
+  )
+}
+
+export default App
